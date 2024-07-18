7b468af0336a4bee56cc318eeab374405e6c3eda
Justin Byrne <justin.byrne@scaffold.digital>
2024-07-18 14:05:22 +0100

    corrected the PDOStub construct to initiate the parent with the correct details

--- tests/Unit/Stubs/PDOStub.php
+++ tests/Unit/Stubs/PDOStub.php
@@ -4,7 +4,16 @@ namespace Stubs;
 
 class PDOStub extends \PDO
 {
-    public function __construct()
+    public function __construct(string $driver = 'mysql')
     {
+        if ($driver !== 'mysql') {
+            return;
+        }
+
+        $dsn = 'mysql:dbname=spatial_test;host=127.0.0.1';
+        $username = 'root';
+        $password = 'password';
+
+        parent::__construct($dsn, $username, $password);
     }
 }
