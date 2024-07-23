1ab3e2717297f1d1e2d9fd45c69fd8b64ece90ea
Justin Byrne <justin.byrne@scaffold.digital>
2024-07-23 12:00:33 +0100

    Changed namespace and which blueprint to use

--- tests/Unit/Schema/BlueprintTest.php
+++ tests/Unit/Schema/BlueprintTest.php
@@ -1,17 +1,14 @@
 <?php
 
-namespace Schema;
+namespace Tests\Unit\Schema;
 
-use BaseTestCase;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\ColumnDefinition;
 use Mockery;
-use ScaffoldDigital\LaravelMysqlSpatial\Schema\Blueprint;
+use Tests\TestCase;
 
-class BlueprintTest extends BaseTestCase
+class BlueprintTest extends TestCase
 {
-    /**
-     * @var \ScaffoldDigital\LaravelMysqlSpatial\Schema\Blueprint
-     */
     protected $blueprint;
 
     public function setUp(): void
